f950bba836319c15bbdcd7ced1a9ff431f855b8d
abdulmoiz01001 <profession.ai01001@hotmail.com>
2025-02-13 01:34:33 +0500

    next-changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,3 +26,16 @@ document.addEventListener("DOMContentLoaded", function () {
         observer.observe(fallingImgSection);
     }
 });
+
+    const swiper = new Swiper('.mySwiper', {
+        loop: true, // Enable infinite loop
+        autoplay: {
+            delay: 0, // No delay between slides
+            disableOnInteraction: false, // Continue autoplay even when user interacts
+        },
+        speed: 5000, // Adjust slide transition speed (in milliseconds)
+        slidesPerView: 'auto', // Automatically adjust slides per view
+        freeMode: true, // Enable free mode for smoother scrolling
+        grabCursor: true, // Show grab cursor on hover
+        spaceBetween: 20, // Space between slides
+    });
